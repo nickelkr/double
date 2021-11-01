2cd9191ed0461c56ca7f4e6b61e2377c460c2e63
Kyle Nickel <1135915+nickelkr@users.noreply.github.com>
2021-10-31 21:25:13 -0400

    Set payload path from CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Opts {
+    #[structopt(parse(from_os_str))]
+    payload_path: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::from_args();
+    println!("Doubling payload @ {:?}", opts.payload_path);
 }
