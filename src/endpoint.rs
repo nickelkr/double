1af5e5f1ee4785e6cb0cb485c5910c1211e84d3a
Kyle Nickel <1135915+nickelkr@users.noreply.github.com>
2021-10-31 22:38:31 -0400

    Formatting

--- src/endpoint.rs
+++ src/endpoint.rs
@@ -1,12 +1,12 @@
-use std::path::PathBuf;
 use std::fs;
+use std::path::PathBuf;
 
 pub struct Endpoint {
     pub payload: String,
 }
 
 impl Endpoint {
-    pub fn from_file(path: PathBuf) -> Result<Endpoint, std::io::Error>  {
+    pub fn from_file(path: PathBuf) -> Result<Endpoint, std::io::Error> {
         let payload = match fs::read_to_string(path) {
             Ok(payload) => payload,
             Err(e) => return Err(e),
