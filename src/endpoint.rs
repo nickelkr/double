05cc27e7b4ebdc4ae9382dcba48b155a2a8bda9a
Kyle Nickel <1135915+nickelkr@users.noreply.github.com>
2021-11-01 21:34:35 -0400

    Pass path to endpoint

--- src/endpoint.rs
+++ src/endpoint.rs
@@ -2,16 +2,17 @@ use std::fs;
 use std::path::PathBuf;
 
 pub struct Endpoint {
+    pub path: String,
     pub payload: String,
 }
 
 impl Endpoint {
-    pub fn from_file(path: PathBuf) -> Result<Endpoint, std::io::Error> {
-        let payload = match fs::read_to_string(path) {
+    pub fn from_file(path: String, payload_path: PathBuf) -> Result<Endpoint, std::io::Error> {
+        let payload = match fs::read_to_string(payload_path) {
             Ok(payload) => payload,
             Err(e) => return Err(e),
         };
 
-        Ok(Endpoint { payload })
+        Ok(Endpoint { path, payload })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ mod endpoint;
 
 #[derive(StructOpt)]
 struct Opts {
+    #[structopt(short, long, default_value = "/")]
+    path: String,
+
     #[structopt(parse(from_os_str))]
     payload_path: PathBuf,
 }
@@ -12,11 +15,12 @@ struct Opts {
 fn main() -> Result<(), std::io::Error> {
     let opts = Opts::from_args();
 
-    let endpoint = match endpoint::Endpoint::from_file(opts.payload_path) {
+    let endpoint = match endpoint::Endpoint::from_file(opts.path, opts.payload_path) {
         Ok(endpoint) => endpoint,
         Err(e) => return Err(e),
     };
 
+    println!("Doubling path: {}", endpoint.path);
     println!("Doubling payload: \n{}", endpoint.payload);
     Ok(())
 }
